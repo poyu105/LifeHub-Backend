b1e9b3ade4386e58725b3b1a4f1df343a9fe0749
poyu <chen06115@gmail.com>
2024-10-03 12:22:55 +0800

    新增更新個人檔案路由

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -8,4 +8,7 @@ router.post('/register', authController.register);
 // 登入路由
 router.post('/login', authController.login);
 
+//更新路由
+router.put('/update/:id', authController.update);
+
 module.exports = router;
